dea61f197f549f8e95ad3d8a0f04833846ce1f00
Nugg3to <marcosbermudez2006@gmail.com>
2026-05-31 17:58:50 +0200

    seeders arreglados

--- database/seeders/FollowsTableSeeder.php
+++ database/seeders/FollowsTableSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 
 class FollowsTableSeeder extends Seeder
